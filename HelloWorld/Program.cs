77c0abefa34ab998380d95a30b22d34f5011360e
euclides981 <euclides981@gmail.com>
2022-10-18 10:40:00 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heloo World");
+            Console.WriteLine("Olá, Digite um número para calcular a tabuada ");
+            String num = Console.ReadLine();
+
+            int numInt = int.Parse(num);
+
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(num + "x" + i + "=" + numInt * i);
+            }
+                Console.ReadLine();
+        }
+    }
+}
